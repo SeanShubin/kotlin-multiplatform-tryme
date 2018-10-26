a79990e943a5fd876af629781cb02f1ae2f00dd0
Sean Shubin <sshubin@cj.com>
2018-10-26 10:25:33 -0700

    refactored

--- jvm/src/main/kotlin/com/seanshubin/tryme/jvm/caclulator/Calculator.kt
+++ jvm/src/main/kotlin/com/seanshubin/tryme/jvm/caclulator/Calculator.kt
@@ -6,48 +6,32 @@ data class Calculator(val cursor: Cursor<Char>) {
     private fun isWord(): Boolean = cursor.valueIs(StringCursor.word)
     private fun isNumber(): Boolean = cursor.valueIs(StringCursor.number)
 
-    private fun addOp(): Calculator {
-        return if (valueIs('+') || valueIs('-')) next()
-        else this
-    }
+    private fun addOp(): Calculator =
+            if (valueIs('+') || valueIs('-')) next()
+            else this
 
-    private fun multOp(): Calculator {
-        return if (valueIs('*') || valueIs('/')) next()
-        else this
-    }
+    private fun multOp(): Calculator =
+            if (valueIs('*') || valueIs('/')) next()
+            else this
 
 
-    private fun factorTail(): Calculator {
-        return if (valueIs('*') || valueIs('/')) {
-            return multOp().factor().factorTail()
-        } else {
-            this
-        }
-    }
+    private fun factorTail(): Calculator =
+            if (valueIs('*') || valueIs('/')) multOp().factor().factorTail()
+            else this
 
-    private fun factor(): Calculator {
-        if (valueIs('(')) {
-            if (next().expr().valueIs(')'))
-                return next()
-            else
-                throw RuntimeException("closing ')' expected")
-        } else if (valueIs('-')) {
-            return next().factor()
-        } else if (isWord())
-            return next();
-        else if (isNumber())
-            return next()
-        else
-            throw RuntimeException("factor expected")
-    }
+    private fun factor(): Calculator =
+            if (valueIs('(')) {
+                val expr = next().expr()
+                if (expr.valueIs(')')) expr.next()
+                else throw RuntimeException("closing ')' expected")
+            } else if (valueIs('-')) next().factor()
+            else if (isWord()) next();
+            else if (isNumber()) next()
+            else throw RuntimeException("factor expected")
 
-    private fun termTail(): Calculator {
-        return if (valueIs('+') || valueIs('-')) {
-            addOp().term().termTail()
-        } else {
-            this
-        }
-    }
+    private fun termTail(): Calculator =
+            if (valueIs('+') || valueIs('-')) addOp().term().termTail()
+            else this
 
     private fun term(): Calculator = factor().factorTail()
 
