00d29294da52574cf2eba2a62d255d0574450c6e
Sean Shubin <sshubin@cj.com>
2019-01-14 16:57:10 -0800

    sequence

--- src/commonMain/kotlin/com/seanshubin/kotlin/tryme/common/matcher/Sequence.kt
+++ src/commonMain/kotlin/com/seanshubin/kotlin/tryme/common/matcher/Sequence.kt
@@ -7,5 +7,19 @@ class Sequence<T>(
     private val lookup: (String) -> Matcher<T>,
     private vararg val matcherNames: String
 ) : Matcher<T> {
-    override fun checkMatch(cursor: Cursor<T>): Result<T> = TODO()
+    override fun checkMatch(cursor: Cursor<T>): Result<T> {
+        var currentCursor = cursor
+        val trees = mutableListOf<Tree<T>>()
+        for (matcherName in matcherNames) {
+            val matcher = lookup(matcherName)
+            val result = matcher.checkMatch(currentCursor)
+            if (result is Failure) {
+                return result
+            }
+            result as Success<T>
+            currentCursor = result.positionAfterMatch
+            trees.add(result.value)
+        }
+        return Success(name, Branch(trees), currentCursor)
+    }
 }
