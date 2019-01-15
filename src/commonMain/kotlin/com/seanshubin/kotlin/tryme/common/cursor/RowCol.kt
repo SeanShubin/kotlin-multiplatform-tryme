e86615fee622373c7df12712c669501d7a029361
Sean Shubin <sshubin@cj.com>
2019-01-15 09:25:24 -0800

    toString for RowCol

--- src/commonMain/kotlin/com/seanshubin/kotlin/tryme/common/cursor/RowCol.kt
+++ src/commonMain/kotlin/com/seanshubin/kotlin/tryme/common/cursor/RowCol.kt
@@ -1,3 +1,5 @@
 package com.seanshubin.kotlin.tryme.common.cursor
 
-data class RowCol(val row: Int, val col: Int)
+data class RowCol(val row: Int, val col: Int) {
+    override fun toString(): String = "[${row + 1}:${col + 1}]"
+}
