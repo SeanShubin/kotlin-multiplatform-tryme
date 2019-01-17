89107f131a95a77cc597a371e09e054281894cb4
Sean Shubin <sshubin@cj.com>
2019-01-17 11:17:03 -0800

    Addition and subtraction

--- src/commonMain/kotlin/com/seanshubin/kotlin/tryme/common/calculator/CalculatorExpressionAssemblers.kt
+++ src/commonMain/kotlin/com/seanshubin/kotlin/tryme/common/calculator/CalculatorExpressionAssemblers.kt
@@ -5,12 +5,29 @@ import com.seanshubin.kotlin.tryme.common.matcher.Branch
 import com.seanshubin.kotlin.tryme.common.matcher.Leaf
 import com.seanshubin.kotlin.tryme.common.matcher.Tree
 
+data class TermPart(val operator: Token, val expression: Expression)
+
 object CalculatorExpressionAssemblers {
     private fun expression(tree: Tree<Token>): Expression {
         require(tree.name == "expression")
         tree as Branch
         require(tree.parts.size == 2)
-        return term(tree.parts[0])
+        val term = term(tree.parts[0])
+        val termTail = termTail(tree.parts[1])
+        val expression = combineTermWithTermTail(term, termTail)
+        return expression
+    }
+
+    private fun combineTermWithTermTail(term: Expression, termTail: List<TermPart>): Expression {
+        var current = term
+        termTail.forEach { termPart ->
+            current = when (termPart.operator) {
+                Token.Plus -> Expression.Plus(current, termPart.expression)
+                Token.Minus -> Expression.Minus(current, termPart.expression)
+                else -> throw RuntimeException("Operator ${termPart.operator} is not supported")
+            }
+        }
+        return current
     }
 
     private fun term(tree: Tree<Token>): Expression {
@@ -18,6 +35,27 @@ object CalculatorExpressionAssemblers {
         tree as Branch
         require(tree.parts.size == 2)
         return number(tree.parts[0])
+    }
+
+
+    private fun termPart(tree: Tree<Token>): TermPart {
+        require(tree.name == "term-part")
+        tree as Branch
+        require(tree.parts.size == 2)
+        val operator = operator(tree.parts[0])
+        val term = term(tree.parts[1])
+        return TermPart(operator, term)
+    }
+
+    private fun termTail(tree: Tree<Token>): List<TermPart> {
+        require(tree.name == "term-tail")
+        tree as Branch
+        return tree.parts.map(::termPart)
+    }
+
+    private fun operator(tree: Tree<Token>): Token {
+        tree as Leaf
+        return tree.value
     }
 
     private fun number(tree: Tree<Token>): Expression {
@@ -48,4 +86,6 @@ object CalculatorExpressionAssemblers {
         }
         return expression
     }
+
+    fun eval(s: String): Int = parse(s).eval()
 }
--- src/commonTest/kotlin/com/seanshubin/kotlin/tryme/common/calculator/CalculatorTest.kt
+++ src/commonTest/kotlin/com/seanshubin/kotlin/tryme/common/calculator/CalculatorTest.kt
@@ -8,12 +8,71 @@ class CalculatorTest {
     fun number() {
         // given
         val input = "123"
-        val expected = Expression.Number(123)
 
         // when
-        val actual = CalculatorExpressionAssemblers.parse(input)
+        val actual = CalculatorExpressionAssemblers.eval(input)
 
         // then
-        assertEquals(expected, actual)
+        assertEquals(123, actual)
+    }
+
+    @Test
+    fun addition() {
+        // given
+        val input = "10 + 5"
+
+        // when
+        val actual = CalculatorExpressionAssemblers.eval(input)
+
+        // then
+        assertEquals(15, actual)
+    }
+
+    @Test
+    fun longChain() {
+        // given
+        val input = "1 + 2 + 3 + 4"
+
+        // when
+        val actual = CalculatorExpressionAssemblers.eval(input)
+
+        // then
+        assertEquals(10, actual)
+    }
+
+    @Test
+    fun subtraction() {
+        // given
+        val input = "10 - 5"
+
+        // when
+        val actual = CalculatorExpressionAssemblers.eval(input)
+
+        // then
+        assertEquals(5, actual)
+    }
+
+    @Test
+    fun multiplication() {
+        // given
+        val input = "2 * 3"
+
+        // when
+        val actual = CalculatorExpressionAssemblers.eval(input)
+
+        // then
+        assertEquals(6, actual)
+    }
+
+    @Test
+    fun division() {
+        // given
+        val input = "6 / 2"
+
+        // when
+        val actual = CalculatorExpressionAssemblers.eval(input)
+
+        // then
+        assertEquals(3, actual)
     }
 }
